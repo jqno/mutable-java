9e946522e4d3aade24cbcfd4b56f942a90330bb9
Jan Ouwens <jan.ouwens@gmail.com>
2016-04-01 00:04:00 +0000

    Refactors to prepare for more types

--- src/main/java/nl/jqno/mutable/java/Mutate.java
+++ src/main/java/nl/jqno/mutable/java/Mutate.java
@@ -4,24 +4,36 @@ import java.lang.reflect.Field;
 
 public class Mutate {
     public static void setInteger(Integer receiver, int newValue) {
-        try {
-            Field field = Integer.class.getDeclaredField("value");
-            field.setAccessible(true);
-            field.set(receiver, newValue);
-        }
-        catch (NoSuchFieldException | IllegalAccessException e) {
-            throw new RuntimeException("It didn't work, I'm sorry :(");
-        }
+        set(Integer.class, "value", receiver, newValue);
     }
 
     public static void setString(String receiver, String newValue) {
         try {
-            Field field = String.class.getDeclaredField("value");
-            field.setAccessible(true);
-            field.set(receiver, field.get(newValue));
+            Field f = getDeclaredField(String.class, "value");
+            set(String.class, "value", receiver, f.get(newValue));
         }
-        catch (NoSuchFieldException | IllegalAccessException e) {
-            throw new RuntimeException("It didn't work, I'm sorry :(");
+        catch (IllegalAccessException | NoSuchFieldException e) {
+            itDidntWork(e);
         }
+    }
+
+    private static <T> void set(Class<T> type, String fieldName, T receiver, Object newValue) {
+        try {
+            Field field = getDeclaredField(type, fieldName);
+            field.set(receiver, newValue);
+        }
+        catch (Exception e) {
+            itDidntWork(e);
+        }
+    }
+
+    private static <T> Field getDeclaredField(Class<T> type, String fieldName) throws NoSuchFieldException {
+        Field field = type.getDeclaredField(fieldName);
+        field.setAccessible(true);
+        return field;
+    }
+
+    private static void itDidntWork(Exception e) {
+        throw new RuntimeException("It didn't work, I'm sorry :(", e);
     }
 }
